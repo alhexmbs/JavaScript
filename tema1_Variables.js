fabffb997656b0f1e8780891072acf06ffc39e10
alhxmbs <alhexmbs@gmail.com>
2024-08-05 17:17:31 -0500

    Primeros pasos

---
+++ tema1_Variables.js
@@ -0,0 +1,6 @@
+//Los comentarios se hacer así
+/*
+Podemos definir a las varialbes de dos formas, con let y con var.
+El problema de definirla con var es el hoisting. (Estudiar hoisting)
+Desde la versión 
+*/
